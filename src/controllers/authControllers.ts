632230687047ed01e13945fcce812c3f5d991286
Durmuş <demirtasdurmus@gmail.com>
2022-07-27 21:10:02 +0300

    TRY models

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -1,22 +1,11 @@
 import { RequestHandler } from 'express';
 import catchAsync from '../utils/catchAsync';
-// import db from "../models";
-// const { User } = db.models
+import db from "../models";
+const { User } = db.models
 
-// console.log("----",)
-// User.create({
-//     firstName: "dddsf",
-//     email: "gfgg.com",
-//     status: "active"
-// })
-//     .then((res) => {
-//         console.log(res)
-//     })
-//     .catch((err) => {
-//         console.log(err)
-//     })
+console.log("----", User)
 export const register: RequestHandler = catchAsync(async (req, res, next) => {
-    // await User.findAll()
+
 });
 
 export const verify: RequestHandler = (req, res, next) => {
